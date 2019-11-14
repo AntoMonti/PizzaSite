447336ca4a0761b7c2337858662f19a2545296cb
AntoMonti <a.matviishyn@hotmail.com>
2019-11-14 22:26:37 +0200

    create cart_get_number_of_items()

--- public/function.js
+++ public/function.js
@@ -7,11 +7,27 @@ function something()
 	
 	}
 
-function add_to_card(id)
+function add_to_cart(id)
 {
 	var key = 'product_' + id;
 
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+}
+
+function cart_get_number_of_items()
+{
+	var cnt = 0;
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);// получаем ключ
+		var value = window.localStorage.getItem(key);// получаем значение, аналог в Руби hh[key] = x
+		if(key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1;
+		}
+	}
+		return cnt;
 }
