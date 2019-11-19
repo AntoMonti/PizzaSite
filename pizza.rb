d84d76db11a5d506fd5321a87791bd17db0002f6
AntoMonti <a.matviishyn@hotmail.com>
2019-11-19 17:47:29 +0200

    parse orders input

--- pizza.rb
+++ pizza.rb
@@ -23,8 +23,30 @@ get '/contacts' do
 end
 
 post '/cart' do
-	erb :cart
+	orders_input = params[:order]
+	@orders = parse_orders_input orders_input
+	erb "#{@orders.inspect}"
 end
+
+def parse_orders_input orders_input
+	s1 = orders_input.split(/,/)
+
+	arr = []
+
+	s1.each do |x|
+		s2 = x.split(/\=/)
+		s3 = s2[0].split(/_/)
+
+		id = s3[1]
+		cnt = s2[1]
+
+		arr2 = [id, cnt]
+		arr.push arr2
+	end
+	return arr
+end
+
+
 
 post '/form' do
 	erb 'thank you'
