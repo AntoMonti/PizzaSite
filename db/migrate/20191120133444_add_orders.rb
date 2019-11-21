2ab6cab3ef2be1a657b5abf9d69701082898a4cb
AntoMonti <a.matviishyn@hotmail.com>
2019-11-21 12:52:50 +0200

    rename add_orders to create_orders, create table

--- db/migrate/20191120133444_add_orders.rb
+++
@@ -1,4 +0,0 @@
-class AddOrders < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_09_161528) do
+ActiveRecord::Schema.define(version: 2019_11_20_133444) do
+
+  create_table "orders", force: :cascade do |t|
+    t.text "order_input"
+    t.text "name"
+    t.text "phone"
+    t.text "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
